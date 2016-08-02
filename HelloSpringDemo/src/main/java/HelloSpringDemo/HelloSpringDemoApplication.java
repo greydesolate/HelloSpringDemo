aa56b304397c850035c9effd5d7495478c323581
GreyDesolate <dzlee@126.com>
2016-08-02 15:47:30 +0800

    Fisrt Commit

---
+++ HelloSpringDemo/src/main/java/HelloSpringDemo/HelloSpringDemoApplication.java
@@ -0,0 +1,12 @@
+package HelloSpringDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringDemoApplication.class, args);
+	}
+}
---
+++ HelloSpringDemo/src/test/java/HelloSpringDemo/HelloSpringDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package HelloSpringDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloSpringDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
